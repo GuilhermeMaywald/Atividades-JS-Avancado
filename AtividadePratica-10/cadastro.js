dd5756336d3159ab8716c8e4199389e8779f05f7
guilherme maywald <guilhermemaywald@gmail.com>
2022-11-21 08:13:56 -0300

    Resolucao Atividade 10 Fetch

---
+++ AtividadePratica-10/cadastro.js
@@ -0,0 +1,45 @@
+
+const btnCadastrar = document.querySelector('#cadastrar');
+const inputNome = document.querySelector('#nome');
+const inputMatricula = document.querySelector('#matricula');
+const divLoadingCadastro = document.querySelector('#loading-cadastro');
+
+btnCadastrar.addEventListener('click', cadastrarAluno);
+
+function cadastrarAluno() {
+    const nomeAluno = inputNome.value;
+    const numeroMatricula = inputMatricula.value;
+
+    if (!nomeAluno || !numeroMatricula) {
+        alert('Dados incorretos');
+        return;
+    }
+
+    divLoadingCadastro.innerHTML = "Carregando...";
+
+    const aluno = {
+        nome: nomeAluno,
+        matricula: numeroMatricula
+    };
+    const initRequest = {
+        method: 'POST',
+        body: JSON.stringify(aluno)
+    };
+
+    const url = 'http://localhost:3000';
+
+    fetch(`${url}/aluno`, initRequest)
+        .then(response => {
+            if (response.ok) {
+                alert('Aluno cadastrado com sucesso');
+                inputNome.value = '';
+                inputMatricula.value = '';
+                divLoadingCadastro.innerHTML = '';
+                return;
+            }
+            throw new Error('Matricula ja cadastrada');
+        })
+        .catch(erro => {
+            alert(erro.message);
+        });
+}
---
+++ AtividadePratica-10/index.js
@@ -0,0 +1,89 @@
+
+const optBuscaNome = document.querySelector("#opt-busca-nome");
+const optBuscaTodos = document.querySelector("#opt-busca-todos");
+const filtersNome = document.querySelector("#pn-filter-nome");
+
+const inputMatricula = document.querySelector('#matricula');
+const btnBuscarMatricula = document.querySelector('#btnBuscarMatricula');
+const labelMatricula = document.querySelector('#matricula-aluno');
+const labelAluno = document.querySelector('#nome-aluno');
+
+const btnBuscarPorNomeOuTodos = document.querySelector('#btnBuscar');
+const divDados = document.querySelector('#dados');
+const divLoadingMatricula = document.querySelector('#loading-matricula');
+
+optBuscaNome.addEventListener("change", (e) => {
+    if (e.target.checked) {
+        filtersNome.style.display = "block"
+    }
+});
+
+optBuscaTodos.addEventListener("change", (e) => {
+    if (e.target.checked) {
+        filtersNome.style.display = "none"
+    }
+});
+
+function buscarMatricula() {
+    const matricula = inputMatricula.value;
+    labelMatricula.innerHTML = '';
+    labelAluno = '';
+    divLoadingMatricula.innerHTML = 'Carregando...';
+
+    const url = 'http://localhost:3000';
+
+    fetch(`${url}/aluno/${matricula}`)
+        .then(response => {
+            if (response.ok) {
+                divLoadingMatricula.innerHTML = '';
+                return response.json();
+            }
+            divLoadingMatricula.innerHTML = '';
+            throw new Error('Matrícula não encontrada!');
+        })
+        .then(jsonData => {
+            labelMatricula.innerHTML = jsonData.matricula;
+            labelAluno.innerHTML = jsonData.nome;
+        })
+        .catch(erro => alert(erro.message));
+}
+
+function buscarNomeOuTodos() {
+    divDados.innerHTML = 'Carregando...';
+    let url = 'http://localhost:3000/aluno/todos';
+
+    if (optBuscaNome.checked) {
+        let nome = document.querySelector('#nome').value;
+        url = `http://localhost:3000/aluno/search?nome=${nome}`;
+    }
+
+    fetch(url).then(response => {
+        if (response.ok) {
+            divDados.innerHTML = '';
+            return response.json();
+        }
+    }).then(jsonData => {
+        if (jsonData.length == 0) {
+            divDados.innerHTML = 'Não foi encontrada essa matricula!';
+            return;
+        }
+        jsonData.forEach(aluno => {
+            divDados.innerHTML += `<p>${aluno.matricula} - ${aluno.nome} <button onClick="excluirAluno(${aluno.matricula})">Excluir Aluno</button></p>`;
+        });
+    });
+}
+
+function excluirAluno(matricula) {
+    const url = `http://localhost:3000/aluno/deletar?matricula=${matricula}`;
+    fetch(url, {
+        method: 'POST'
+    }).then (response => {
+        if (response.ok) {
+            return buscarNomeOuTodos();
+        }
+        throw new Error('Matrícula não encontrada!');
+    }).catch(error => alert(error));
+}
+
+btnBuscarMatricula.addEventListener('click', buscarMatricula);
+btnBuscarPorNomeOuTodos.addEventListener('click', buscarNomeOuTodos);
